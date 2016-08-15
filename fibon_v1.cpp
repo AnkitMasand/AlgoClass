638c795f442837d56baac4790b020b3450d1e6e1
suparna-ghanvatkar <suparnaghanvatkar@yahoo.co.in>
2016-08-16 00:32:13 +0530

    working within 4 mins

--- fibon_v1.cpp
+++ fibon_v1.cpp
@@ -1,6 +1,10 @@
 //Program to calculate F(n) for F(n)=(F(n-1)+F(n-2))mod m
 #include<iostream>
+#include<fstream>
+#include<cstring>
 #include<cstdio>
+#include<sys/stat.h>
+using namespace std;
 int A[2][2]={1,1,
 			 1,0};
 int y[2][2]={1,0,
@@ -9,25 +13,33 @@ int NZero=1;
 int pow(short*,int,int);
 void MatMul(int (&dest)[2][2],int mat[2][2],int m);
 void Divby2(short*,int);
+int start=0;
 using namespace std;
-int main(){
+int main(int argc,char *argv[]){
 	int m;
-	short n[1000000];
+	char cn[1000100];
 	int nlen=0;
 	char temp;
-	for(int i=0;;i++){
-		scanf("%c",&temp);
-		if(temp=='\n'){
+	ifstream f;
+	struct stat buffer1;
+	f.open(argv[1],ios::in);
+	f>>cn;
+	stat(argv[1],&buffer1);
+	short n[buffer1.st_size];
+	for(int i=0;i<buffer1.st_size;i++){
+		if(cn[i]=='\0')
 			break;
-		}
 		else{
-			nlen++;
-			n[i]=temp-'0';
+			n[i]=cn[i]-'0';
 			if(n[i]!=0)
 				NZero=0;
+			nlen++;
 		}
 	}
-	cin>>m;
+	//cin>>m;
+	m=atoi(argv[2]);
+	while(n[start]==0)
+		start++;
 	cout<<pow(n,nlen,m);
 	return 0;
 }
@@ -37,7 +49,7 @@ int pow(short *n,int len,int m){
 		if(n[len-1]%2==1)
 		{
 			MatMul(y,A,m);
-			//cout<<"ymul"<<" "<<y[0][0]<<y[0][1]<<y[1][0]<<y[1][1]<<"\n";
+			cout<<"ymul"<<" "<<y[0][0]<<y[0][1]<<y[1][0]<<y[1][1]<<"\n";
 		}
 		MatMul(A,A,m);
 		//cout<<"mul"<<" "<<A[0][0]<<A[0][1]<<A[1][0]<<A[1][1]<<"\n";
@@ -62,12 +74,16 @@ void Divby2(short *n,int len){
 	int pcarry=0;
 	int carry;
 	int zero=1;
-	for(int i=0;i<len;i++){
-		carry=((pcarry*10)+n[i])%2;
-		n[i]=((pcarry*10)+n[i])/2;
+	int d;
+	for(int i=start;i<len;i++){
+		d=(pcarry*10)+n[i];
+		carry=(d)%2;
+		n[i]=(d)/2;
 		pcarry=carry;
 		if(n[i]!=0)
 			zero=0;
 	}
+	if(n[start]==0)
+		start++;
 	NZero=zero;
 }
