43a89a3aeb98a6912b5ae02e876e23fff2be4fe0
suparna-ghanvatkar <suparnaghanvatkar@yahoo.co.in>
2016-09-03 23:25:25 +0530

    random rank done...checking of all left

--- randrank.cpp
+++ randrank.cpp
@@ -1,41 +1,55 @@
 #include<iostream>
+#include<cstdio>
 #include<cstdlib>
+#include<algorithm>
 #define MAXLEN 1000000
 int len;
 using namespace std;
-int partition(int *A,int l,int r, int p){
+
+int partition(int *A,int l,int r, int pi){
 	int i=l;
-	int j=r;
+	int p=A[pi];	//pivot p is at pivtindex
+	swap(A[pi],A[r]);
+	int j=r-1;
+	int pivotindex; // pivot p in our case exsists in the array
 	while(i<j){
 		while(A[i]<p)
 			i++;
-		while(A[j]>p)
+		while(A[j]>=p)
 			j--;
 		if(i<j){
-			//cout<<"swapping "<<A[i]<<" "<<A[j]<<endl;
-			A[i]+=A[j];
-			A[j]=A[i]-A[j];
-			A[i]=A[i]-A[j];
+			swap(A[i],A[j]);
 			i++; j--;
 		}
-		//cout<<"i:"<<i<<"j:"<<j<<endl;
+	}
+	for(int l=0;l<len;l++)
+		cout<<A[l]<<" ";
+	cout<<"swapping j+1 and r:"<<j+1<<" "<<r<<endl;
+	if(A[j]>=p){
+		swap(A[j],A[r]);
+	}
+	else{
+		swap(A[j+1],A[r]);
+		j++;
 	}
 	return j;
 }
+
 int findRank(int *A,int i, int j, int r){
-	int p=A[i+(rand()%(j-i+1))];//RANDOM PIVOT 
-	//cout<<"pivot:"<<p<<endl;
+	//pivot is passed as index of the element only!
+	int p=i + (rand()%(j-i+1));	//deterministic pivot selection 
+	cout<<"pivot:"<<p<<endl;
 	int k=partition(A,i,j,p);
-	//cout<<"partition:"<<k<<"i:"<<i<<"j:"<<j<<"r:"<<r<<endl;
-	/*for(int l=i;l<=j;l++)
-		cout<<A[l]<<" ";*/
-	//cout<<endl;
+	cout<<"partition:"<<k<<"i:"<<i<<"j:"<<j<<"r:"<<r<<endl;
+	for(int l=0;l<len;l++)
+		cout<<A[l]<<" ";
+	cout<<endl;
 	if(r==(j-k+1))
-		return p;
+		return k;
 	else if(r<(j-k+1))
-		findRank(A,k+1,j,r);
+		return findRank(A,k+1,j,r);
 	else
-		findRank(A,i,k-1,r-j+k-1);
+		return findRank(A,i,k-1,r-j+k-1);
 }
 int main(){
 	cin>>len;
@@ -44,12 +58,12 @@ int main(){
 	for(int i=0;i<len;i++)
 		cin>>A[i];
 	int m;
-	cin>>m;
+	cin>>m;	//no of ranks to be calculated
 	int rank;
 	for(int i=0;i<m;i++)
 	{
 		cin>>rank;
-		ele=findRank(A,0,len-1,rank);
+		ele=A[findRank(A,0,len-1,rank)];
 		cout<<ele<<endl;
 	}
 }
