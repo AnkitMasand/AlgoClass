d82cfad5f9b42d3ef265c58b51c966232927cb8a
suparna-ghanvatkar <suparnaghanvatkar@yahoo.co.in>
2016-11-04 16:14:51 +0530

    perfect hash complete

--- perfect hashing/perfecthash.cpp
+++ perfect hashing/perfecthash.cpp
@@ -4,7 +4,6 @@
 #include<fstream>
 #include<string>
 #define MIN -999999
-#define SIZE 1502
 
 using namespace std;
 
@@ -23,15 +22,17 @@ class Bucket{
 	vector<long long int> sec;
    public:
 	Bucket(){
-		prime=991;
+		prime=6700417;
 		a=rand()%prime;
 		b=rand()%prime;
 		while(b==a)
 			b=rand()%prime;	//to ensure different a and b	
 		m=0;	//size 0 initially
 	}
-	void add(vector<long long int> &ele,int len);
+	int add(vector<long long int> &ele,int len);
 	void display();
+	void lookup(long long int);
+	void diff1();
 };
 
 class Table{
@@ -41,39 +42,89 @@ class Table{
 	int size;
 	int prime_p;
    public:
-	Table(){
-		pri.assign(SIZE,Bucket());
-		prime_p=997;;
+	Table(int Size){
+		pri.assign(Size,Bucket());
+		prime_p=2147483647;;
 		a=rand()%prime_p;
 		b=rand()%prime_p;
 		while(b==a)
 			b=rand()%prime_p;
-		size=SIZE;	
+		size=Size;	
 	}
 	void add(char *filenm,int n);
 	void display();
+	void lookup(long long int);
+	void diff1();
 };
 
-void Bucket::add(vector<long long int> &ele, int len){
+void Table::diff1(){
+	for(int i=0;i<size;i++){
+		pri[i].diff1();
+	}
+}
+
+void Bucket::diff1(){
+	for(int i=0;i<m;i++){
+		if(sec[i]!=MIN){
+			for(int j=0;j<m;j++)
+			{
+				if(sec[j]==(sec[i]+1))
+					cout<<sec[i]<<" and "<<sec[j]<<endl;
+			}
+		}
+	}	
+}
+	
+void Bucket::lookup(long long int val){
+	if(m==0)
+	{
+		cout<<"Element not found!"<<endl;
+		return;
+	}
+	int hashkey=hashh(val,a,b,prime,m);
+	cout<<"sec hash:"<<hashkey<<" "<<m<<endl;
+	if(sec[hashkey]==val)
+		cout<<"Element found in location :"<<hashkey<<" of secondary hash table"<<endl;
+	else
+		cout<<"Element not found in secondary hash!"<<endl;
+}
+
+void Table::lookup(long long int val){
+	int hashkey=hashh(val,a,b,prime_p,size);
+	cout<<"Primary hash to bucket number:"<<hashkey<<endl;
+	if(hashkey<0){
+		cout<<"Element not found!"<<endl;
+		return;
+	}
+	pri[hashkey].lookup(val);
+}
+
+int Bucket::add(vector<long long int> &ele, int len){
 	m=len*len;
 	sec.clear();
 	sec.assign(m,MIN);	
 	int hashh_key,flag=0;// flag 0 for starting for loop
-	rehashh:
 	for(int i=0;i<len;i++){
 		flag=1;
 		hashh_key=hashh(ele[i],a,b,prime,m);
 		if(sec[hashh_key]!=MIN)
 		{
+			cout<<"elem present:"<<sec[hashh_key]<<" nd to be hadshd:"<<ele[i]<<" "<<a<<" "<<b<<" "<<m<<endl;
+			if(sec[hashh_key]==ele[i]){
+				cout<<"repetition:"<<ele[i]<<endl;
+				continue;
+			}
 			a=rand()%prime;
+			b=rand()%prime;
 			while(b==a)
 				b=rand()%prime;
-			sec.clear();
-			sec.assign(m,MIN);
-			goto rehashh;
+			cout<<"need to rehash!"<<endl;
+			//sec.assign(m,MIN);
+			return -1;
 		}
 		sec[hashh_key]=ele[i];	
-	}	
+	}
+	return 0;	
 }
 
 void Table::add(char *filenm,int n){
@@ -85,18 +136,20 @@ void Table::add(char *filenm,int n){
 	string str;
 	for(int i=0;i<n;i++){
 		fp>>str;
-		cout<<str;
+		cout<<i<<" ";
 		ele=stoll(str);		
 		hashh_key=hashh(ele,a,b,prime_p,size);
 		eles[hashh_key].push_back(ele);
 	}
+	cout<<"finished reading!"<<endl;
+	long sum=0;
 	for(int i=0;i<n;i++)
 	{
-		if((2*n)< (eles[i].size() * eles[i].size()))
-		{
-			flag=0;
-			break;
-		}
+		sum+=(eles[i].size() * eles[i].size());
+	}
+	if((2*n) < sum){
+		flag=0;
+		cout<<"rehash pri"<<endl;
 	}
 	if(flag==0){
 		a=rand()%prime_p;
@@ -106,10 +159,14 @@ void Table::add(char *filenm,int n){
 		add(filenm,n);
 		return;	
 	}
-	else{
+	else{	
+		int status;
 		pri.assign(size,Bucket());
 		for(int i=0;i<size;i++){
-			pri[i].add(eles[i],eles[i].size());
+			cout<<"adding to bucket no:"<<i<<endl;
+			status=pri[i].add(eles[i],eles[i].size());
+			while(status==-1)
+				status=pri[i].add(eles[i],eles[i].size());
 		}			
 	}
 	fp.close();
@@ -130,9 +187,19 @@ void Table::display(){
 }		
 
 int main(int argc, char *argv[]){
-	Table T;
 	int no_of_elems;
 	no_of_elems=atoi(argv[1]);
+	Table T(no_of_elems);
 	T.add(argv[2],no_of_elems);
-	T.display();
+	//T.display();
+	cout<<"Elemenets to be searched:"<<endl;
+	long long int key;
+	for(int i=0;i<2;i++)
+	{
+		cin>>key;
+		T.lookup(key);
+	}
+	cout<<"Pairs with diff 1:"<<endl;
+	T.diff1();
+	return 0;
 }
